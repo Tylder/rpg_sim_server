92fd0e1561b76239476f3511fdc97d77f72f3408
Daniel Lofgren <lofgrendaniel@hotmail.com>
2022-05-28 23:30:22 +0900

    wip: entities in components

---
+++ flecs_modules/transform/components.cpp
@@ -0,0 +1,356 @@
+//
+// Created by Anon on 5/27/2022.
+//
+
+#ifndef RPG_SIM_SERVER_TRANSFORM_COMPONENTS_H
+#define RPG_SIM_SERVER_TRANSFORM_COMPONENTS_H
+
+#include <string>
+#include "flecs.h"
+
+namespace Transform {
+
+//template<typename T = double>
+//void registerTransform2Dim(flecs::world &ecsWorld, const std::string& name) {
+//  ecsWorld.component<double>(name)
+//      .member<T>("x")
+//      .member<T>("y");
+//}
+
+template<typename T = double>
+struct Tranform2Dim {
+  T x, y;
+};
+
+template<typename T = double>
+struct Tranform3Dim {
+  T x, y, z;
+};
+
+/* Position */
+template<typename T = double>
+struct Size2 : Tranform2Dim<T> {};
+
+template<typename T = double>
+struct Size3 : Tranform3Dim<T> {};
+
+/* Position */
+template<typename T = double>
+struct Position2 : Tranform2Dim<T> {};
+
+template<typename T = double>
+struct Position3 : Tranform3Dim<T> {};
+
+/* Velocity */
+template<typename T = double>
+struct Velocity1 {
+  T value;
+};
+
+template<typename T = double>
+struct Velocity2 : Tranform2Dim<T> {};
+
+template<typename T = double>
+struct Velocity3 : Tranform3Dim<T> {};
+
+template<typename T = double>
+struct Rotation1 {
+  T angle;
+};
+
+/* Rotation */
+template<typename T = double>
+struct Rotation3 : Tranform3Dim<T> {};
+
+template<typename T = double>
+struct Quaternion {
+  T x, y, z, w;
+};
+
+/* Scale */
+template<typename T = double>
+struct Scale1 {
+  T value;
+};
+
+template<typename T = double>
+struct Scale2 : Tranform2Dim<T> {};
+
+template<typename T = double>
+struct Scale3 : Tranform3Dim<T> {};
+
+struct Componets {
+  Componets(flecs::world &ecsWorld) {
+    ecsWorld.module<Transform::Componets>();
+
+    /* REGISTERING COMPONENTS FOR REFLECTION */
+
+    /* SIZE 2 */
+    ecsWorld.component<Size2<double>>("Size2")
+        .member<double>("x")
+        .member<double>("y");
+
+    ecsWorld.component<Size2<float>>("Size2<float>")
+        .member<float>("x")
+        .member<float>("y");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Size2<int32_t>>("Size2<int32_t>")
+        .member(flecs::I32, "x")
+        .member(flecs::I32, "y");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Size2<int64_t>>("Size2<int64_t>")
+        .member(flecs::I64, "x")
+        .member(flecs::I64, "y");
+
+    /* SIZE 3 */
+    ecsWorld.component<Size3<double>>("Size3")
+        .member<double>("x")
+        .member<double>("y")
+        .member<double>("z");
+
+    ecsWorld.component<Size3<float>>("Size3<float>")
+        .member<float>("x")
+        .member<float>("y")
+        .member<float>("z");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Size3<int32_t>>("Size3<int32_t>")
+        .member(flecs::I32, "x")
+        .member(flecs::I32, "y")
+        .member(flecs::I32, "z");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Size3<int64_t>>("Size3<int64_t>")
+        .member(flecs::I64, "x")
+        .member(flecs::I64, "y")
+        .member(flecs::I64, "z");
+
+
+    /* POSITION 2 */
+    ecsWorld.component<Position2<double>>("Position2")
+        .member<double>("x")
+        .member<double>("y");
+
+    ecsWorld.component<Position2<float>>("Position2<float>")
+        .member<float>("x")
+        .member<float>("y");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Position2<int32_t>>("Position2<int32_t>")
+        .member(flecs::I32, "x")
+        .member(flecs::I32, "y");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Position2<int64_t>>("Position2<int64_t>")
+        .member(flecs::I64, "x")
+        .member(flecs::I64, "y");
+
+    /* POSITION 3 */
+    ecsWorld.component<Position3<double>>("Position3")
+        .member<double>("x")
+        .member<double>("y")
+        .member<double>("z");
+
+    ecsWorld.component<Position3<float>>("Position3<float>")
+        .member<float>("x")
+        .member<float>("y")
+        .member<float>("z");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Position3<int32_t>>("Position3<int32_t>")
+        .member(flecs::I32, "x")
+        .member(flecs::I32, "y")
+        .member(flecs::I32, "z");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Position3<int64_t>>("Position3<int64_t>")
+        .member(flecs::I64, "x")
+        .member(flecs::I64, "y")
+        .member(flecs::I64, "z");
+
+    /* VELOCITY 1 */
+    ecsWorld.component<Velocity1<double>>("Velocity1")
+        .member<double>("value");
+
+    ecsWorld.component<Velocity1<float>>("Velocity1<float>")
+        .member<float>("value");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Velocity1<int32_t>>("Velocity1<int32_t>")
+        .member(flecs::I32, "value");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Velocity1<int64_t>>("Velocity1<int64_t>")
+        .member(flecs::I64, "value");
+
+    /* VELOCITY 2 */
+    ecsWorld.component<Velocity2<double>>("Velocity2")
+        .member<double>("x")
+        .member<double>("y");
+
+    ecsWorld.component<Velocity2<float>>("Velocity2<float>")
+        .member<float>("x")
+        .member<float>("y");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Velocity2<int32_t>>("Velocity2<int32_t>")
+        .member(flecs::I32, "x")
+        .member(flecs::I32, "y");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Velocity2<int64_t>>("Velocity2<int64_t>")
+        .member(flecs::I64, "x")
+        .member(flecs::I64, "y");
+
+    /* VELOCITY 3 */
+    ecsWorld.component<Velocity3<double>>("Velocity3")
+        .member<double>("x")
+        .member<double>("y")
+        .member<double>("z");
+
+    ecsWorld.component<Velocity3<float>>("Velocity3<float>")
+        .member<float>("x")
+        .member<float>("y")
+        .member<float>("z");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Velocity3<int32_t>>("Velocity3<int32_t>")
+        .member(flecs::I32, "x")
+        .member(flecs::I32, "y")
+        .member(flecs::I32, "z");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Velocity3<int64_t>>("Velocity3<int64_t>")
+        .member(flecs::I64, "x")
+        .member(flecs::I64, "y")
+        .member(flecs::I64, "z");
+
+    /* ROTATION 1 */
+    ecsWorld.component<Rotation1<double>>("Rotation1")
+        .member<double>("value");
+
+    ecsWorld.component<Rotation1<float>>("Rotation1<float>")
+        .member<float>("value");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Rotation1<int32_t>>("Rotation1<int32_t>")
+        .member(flecs::I32, "value");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Rotation1<int64_t>>("Rotation1<int64_t>")
+        .member(flecs::I64, "value");
+
+    /* ROTATION 3 */
+    ecsWorld.component<Rotation3<double>>("Rotation3")
+        .member<double>("x")
+        .member<double>("y")
+        .member<double>("z");
+
+    ecsWorld.component<Rotation3<float>>("Rotation3<float>")
+        .member<float>("x")
+        .member<float>("y")
+        .member<float>("z");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Rotation3<int32_t>>("Rotation3<int32_t>")
+        .member(flecs::I32, "x")
+        .member(flecs::I32, "y")
+        .member(flecs::I32, "z");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Rotation3<int64_t>>("Rotation3<int64_t>")
+        .member(flecs::I64, "x")
+        .member(flecs::I64, "y")
+        .member(flecs::I64, "z");
+
+    /* QUATERNION */
+    ecsWorld.component<Quaternion<double>>("Quaternion")
+        .member<double>("x")
+        .member<double>("y")
+        .member<double>("z")
+        .member<double>("w");
+
+    ecsWorld.component<Quaternion<float>>("Quaternion<float>")
+        .member<float>("x")
+        .member<float>("y")
+        .member<float>("z")
+        .member<float>("w");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Quaternion<int32_t>>("Quaternion<int32_t>")
+        .member(flecs::I32, "x")
+        .member(flecs::I32, "y")
+        .member(flecs::I32, "z")
+        .member(flecs::I32, "w");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Quaternion<int64_t>>("Quaternion<int64_t>")
+        .member(flecs::I64, "x")
+        .member(flecs::I64, "y")
+        .member(flecs::I64, "z")
+        .member(flecs::I64, "w");
+
+    /* SCALE 1 */
+    ecsWorld.component<Scale1<double>>("Scale1")
+        .member<double>("value");
+
+    ecsWorld.component<Scale1<float>>("Scale1<float>")
+        .member<float>("value");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Scale1<int32_t>>("Scale1<int32_t>")
+        .member(flecs::I32, "value");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Scale1<int64_t>>("Scale1<int64_t>")
+        .member(flecs::I64, "value");
+
+    /* SCALE 2 */
+    ecsWorld.component<Scale2<double>>("Scale2")
+        .member<double>("x")
+        .member<double>("y");
+
+    ecsWorld.component<Scale2<float>>("Scale2<float>")
+        .member<float>("x")
+        .member<float>("y");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Scale2<int32_t>>("Scale2<int32_t>")
+        .member(flecs::I32, "x")
+        .member(flecs::I32, "y");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Scale2<int64_t>>("Scale2<int64_t>")
+        .member(flecs::I64, "x")
+        .member(flecs::I64, "y");
+
+    /* SCALE 3 */
+    ecsWorld.component<Scale3<double>>("Scale3")
+        .member<double>("x")
+        .member<double>("y")
+        .member<double>("z");
+
+    ecsWorld.component<Scale3<float>>("Scale3<float>")
+        .member<float>("x")
+        .member<float>("y")
+        .member<float>("z");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Scale3<int32_t>>("Scale3<int32_t>")
+        .member(flecs::I32, "x")
+        .member(flecs::I32, "y")
+        .member(flecs::I32, "z");
+
+    // Use platform-independent type ids
+    ecsWorld.component<Scale3<int64_t>>("Scale3<int64_t>")
+        .member(flecs::I64, "x")
+        .member(flecs::I64, "y")
+        .member(flecs::I64, "z");
+
+  }
+};
+}
+
+#endif // RPG_SIM_SERVER_TRANSFORM_COMPONENTS_H
--- main.cpp
+++ main.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
-#include "FastNoise/FastNoise.h"
-#include "flecs.h"
 #include <chrono>
 
-/* Component types */
+#include "FastNoise/FastNoise.h"
+#include "flecs.h"
+#include "flecs_modules/landscape/components.cpp"
+#include "flecs_modules/landscape/systems.cpp"
+#include "flecs_modules/transform/components.cpp"
+#include "flecs_modules/transform/systems.cpp"
+
 struct Position {
-    double x, y;
-};
-
-struct Velocity {
-    double x, y;
-};
-
-struct Crap {
-    int foo[1000]; // meant to mess up the cache by taking up useless space
+  float x;
+  float y;
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+  std::cout << "Hello, World!" << std::endl;
 //    std::vector<float> noiseOutput(16 * 16 * 16);
 
 //    auto fnSimplex = FastNoise::New<FastNoise::Simplex>();
@@ -29,62 +26,121 @@ int main() {
 //    // Generate a 16 x 16 x 16 area of noise
 //    fnSimplex->GenUniformGrid3D(noiseOutput.data(), 0, 0, 0, 16, 16, 16, 0.2f, 1337);
 
+#ifdef FLECS_DEBUG
+  flecs::log::set_level(10);
+#endif
+  flecs::world ecs;
+  ecs.import<Landscape::Components>();
+  ecs.import<Landscape::Systems>();
+  ecs.import<Transform::Componets>();
+  ecs.import<Transform::Systems>();
+//    ecs.app().enable_rest().run();
 
-    flecs::world ecs;
 
-    #ifdef FLECS_DEBUG
-    flecs::log::set_level(10);
-    #endif
+  ecs.set_threads(4);
 
-    ecs.set_threads(4);
+  Landscape::addLandscapeTile(ecs, 2, 2);
 
-    ecs.system<Position, const Velocity>()
-//        .interval(1)
-        .multi_threaded(false)
-        .iter([](const flecs::iter& it,Position *p,const Velocity *v){
-            for (auto row : it) {
-                    p[row].x += v[row].x;
-                    p[row].y += v[row].y;
-                }
-//            std::cout << 'end of system 1' << std::endl;
-        });
+  auto qTile = ecs.query<const Landscape::LandscapeTile, Transform::Position2<int32_t>, Landscape::Neighbours8>();
 
-//    ecs.system<Position>()
-//            .iter([](const flecs::iter& it,Position *p){
-//                for (auto row : it) {
-//                    p[row].x += 1;
-//                    p[row].y += 1;
+  flecs::iter_to_json_desc_t descIter = {};
+  descIter.serialize_entities = true;
+  descIter.serialize_values = true;
+
+  std::cout << qTile.iter().to_json(&descIter).c_str() << "\n";
+//
+//  auto e = flecs::entity(ecs, 999)
+//      .set<Transform::Position2<int32_t>>({4, 11})
+//      .set<Transform::Position2<>>({4, 11})
+//      .set<Transform::Position3<>>({2.1, 3.3, 5.5})
+//      .set<Transform::Velocity2<>>({1, 1})
+//      .set<Transform::Velocity3<>>({3, 3});
+//
+//  const auto *ptr = e.get<Transform::Position2<int32_t>>();
+//  const auto *ptr2 = e.get<Transform::Position2<>>();
+//  const auto exp = std::string(ecs.to_expr(ptr).c_str());
+//  const auto exp2 = std::string(ecs.to_expr(ptr2).c_str());
+////  const auto json = std::string(ecs.to_json(ptr).c_str());
+////  const auto json2 = std::string(ecs.to_json(ptr2).c_str());
+////  const auto jsonE = std::string(e.to_json().c_str());
+//
+////  auto q = ecs.query<Transform::Position2<>, const Transform::Position3<>>();
+//
+//  // Serialize query to JSON. Customize serializer to only serialize entity
+//  // names and component values.
+//
+//
+//  const auto json = std::string(ecs.to_json(ptr).c_str());
+//  const auto json2 = std::string(ecs.to_json(ptr2).c_str());
+//
+//  flecs::entity_to_json_desc_t descEnt = {};
+//  descEnt.serialize_values = true;
+//  descEnt.serialize_base = true;
+//  descEnt.serialize_brief = true;
+//  descEnt.serialize_hidden = true;
+//  descEnt.serialize_label = true;
+//  descEnt.serialize_link = true;
+//  descEnt.serialize_path = true;
+//  descEnt.serialize_values = true;
+//  descEnt.serialize_type_info = true;
+//
+//  auto jsonE = std::string(e.to_json(&descEnt).c_str());
+//  std::cout << jsonE << std::endl;
+
+//
+
+//
+
+//
+//    ecs.system<Position2, const Velocity2>()
+////        .interval(1)
+//        .multi_threaded(false)
+//        .iter([](const flecs::iter& it,Position2 *p,const Velocity2 *v){
+//            for (auto row : it) {
+//                    p[row].x += v[row].x;
+//                    p[row].y += v[row].y;
 //                }
-////                std::cout << 'end of system 2' << std::endl;
-//            });
-
-    auto entity = ecs.entity("myEntity")
-            .set<Position>({0, 0})
-            .set<Velocity>({1, 1});
-
-
-
-    for (int i = 0; i < 10000; ++i) {
-        ecs.entity()
-                .set<Position>({0, 0})
-                .set<Velocity>({1, 1})
-                .set<Crap>({0});
-
-    }
+////            std::cout << 'end of system 1' << std::endl;
+//        });
+//
+////    ecs.system<Position>()
+////            .iter([](const flecs::iter& it,Position *p){
+////                for (auto row : it) {
+////                    p[row].x += 1;
+////                    p[row].y += 1;
+////                }
+//////                std::cout << 'end of system 2' << std::endl;
+////            });
+//
+//    auto entity = ecs.entity("myEntity")
+//            .set<Position2>({0, 0})
+//            .set<Velocity2>({1, 1});
+//
+//
+//
+//  for (int i = 0; i < 2; ++i) {
+//    ecs.entity()
+//        .set<Transform::Position2<>>({0, 0})
+//        .set<Transform::Velocity2<>>({1, 1});
+//  }
 //
 //
 //
 ////    ecs.set_target_fps(1);
 
-    std::cout << "Application move_system is running, press CTRL-C to exit..." << std::endl;
+  std::cout << "Application move_system is running, press CTRL-C to exit..." << std::endl;
 
-    /* Run systems */
-    typedef std::chrono::high_resolution_clock Clock;
-    auto t1 = Clock::now();
+  /* Run systems */
+  typedef std::chrono::high_resolution_clock Clock;
+  auto t1 = Clock::now();
 
-    for (int i = 0; i < 1000000; ++i) {
-        ecs.progress();
-    }
+//    while (ecs.progress()) {
+//
+//    }
+
+  for (int i = 0; i < 1; ++i) {
+    ecs.progress();
+  }
 
 //    while (ecs.progress()) {
 //        std::cout << "Delta time: " << ecs.get_world().delta_time() << std::endl;
@@ -92,9 +148,12 @@ int main() {
 ////                  entity.get<Position>()->x << ", " << entity.get<Position>()->y << "} threads: " << ecs.get_threads()  <<     std::endl;
 //    }
 
-    auto t2 = Clock::now();
-    std::chrono::duration<double, std::milli> fp_ms = t2 - t1;
-    std::cout << fp_ms << '\n';
+  auto t2 = Clock::now();
+  std::chrono::duration<double, std::milli> fp_ms = t2 - t1;
+  std::cout << fp_ms.count() << '\n';
 
-    return 0;
+//  auto jsonE2 = std::string(e.to_json(&descEnt).c_str());
+//  std::cout << jsonE2 << std::endl;
+
+  return 0;
 }
